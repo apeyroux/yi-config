18fbe246e8cbfddd438b3b680bc9bd1e69b3e79e
Alexandre Peyroux <alex@xn--wxa.email>
2017-07-27 00:26:43 +0200

    add shellHook

--- shell.nix
+++ shell.nix
@@ -4,5 +4,8 @@ with pkgs;
 
 stdenv.mkDerivation {
   name = "yi-config";
-  buildInputs = [ haskellPackages.stack haskellPackages.cabal-install icu.out haskell.compiler.ghc802 ncurses.out ];
+  buildInputs = [ haskellPackages.stack haskellPackages.cabal-install icu haskell.compiler.ghc802 ncurses ];
+  shellHook = ''
+    export LD_LIBRARY_PATH="${ncurses}/lib:${icu}/lib:$LD_LIBRARY_PATH"
+  ''; 
 }
